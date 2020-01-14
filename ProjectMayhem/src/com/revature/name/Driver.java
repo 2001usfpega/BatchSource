d1d649f924ee7a34fdc0d35f53649005ca99cac0
matt <jason.knighten@revature.com>
2020-01-14 13:08:58 -0500

    Added ProjectMayhem

---
+++ ProjectMayhem/src/com/revature/name/Driver.java
@@ -0,0 +1,24 @@
+package com.revature.name;
+
+public class Driver {
+	//single line comment
+	/*
+	 * multi line
+	 * comment
+	 *
+	 * Naming Conventions
+	 * Classes and Projects - Pascal casing ex. ProjectMayhem
+	 * Methods and Variables - Camel casing ex. firstSecondThird
+	 *
+	 * package - all lower case, separated by periods
+	 * constants - ALL_CAPS_LOLZ
+	 */
+	//Main method - serves as the entry point for an application
+	public static void main(String [] args) {
+		//public- access modifiers errbody can "see" it
+		//static- belongs to class, no object needed
+		// void- doesn't return anything
+		// main- name of method
+		// String [] args-  an array of Strings called args
+	}
+}
