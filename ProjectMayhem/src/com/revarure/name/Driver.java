fcb23c7eed56e18434a9415700f3d5b76fb2a0de
dungcules <dunganh.le1988@gmail.com>
2020-01-14 13:30:10 -0500

    new files

---
+++ ProjectMayhem/src/com/revarure/name/Driver.java
@@ -0,0 +1,36 @@
+package com.revarure.name;
+
+public class Driver {
+
+	// Single line comment
+	/*
+	 * Multi line comment
+	 * 
+	 * Naming Conventions Classes and Projects - Pascal casing ex. ProjectMayhem
+	 * Methods and variables - Camel casing ex firstSecondThird 
+	 * Package - all lower case, separated by periods 
+	 * Constant - ALL_CAPS_LOLZ
+	 */
+	
+	//Inherit, Abstract, ..., ... class
+	
+	
+
+	// Main Method - serves as the entry point for an application
+	public static void main(String[] args) {
+		// public - access modifiers everybody can see it
+		// static - belong to the class, no object needed
+		// void - does not return anything
+		// main - name of the method
+		// String [] args - an array of Strings called args
+
+		int a = 3;
+		int b = 6;
+		int c = a + b;
+
+		// Short key: sysout + Ctrl + Space
+		System.out.println("I like to eat "+c+" tacos");
+
+	}
+
+}
