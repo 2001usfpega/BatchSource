01937a36db87a0685e5aa1eff0dc077eb1ab5641
nexxgodd <crazy_nuts_2006@yahoo.com>
2020-01-14 13:33:53 -0500

    created projectmaham

---
+++ ProjectMayham/src/com/revature/name/Driver.java
@@ -0,0 +1,34 @@
+package com.revature.name;
+
+public class Driver {
+	
+	//single
+	
+	/*
+	 * multi
+	 * line
+	 * 
+	 * Classes and projects are pascal case ex ProjectMayham
+	 * methods and variable are camel case  ex oneTwoThree
+	 * package lower seperated by dots		ex com.revature
+	 * constants are all caps				ex PI_PLUS_TWO
+	 * 
+	 */
+	
+	//
+	public static void main(String[] args) {
+		/* public- visable by all
+		 * static- belongs to the class, doesnt need an object
+		 * void- doesnt return
+		 * main- name of method
+		 * String[] args- array of strings
+		 */
+		
+		int a=3;
+		int b=46;
+		int c=a+b;
+		System.out.println("i like to eat " + c + " tacos");
+		
+	}
+	
+}
---
+++ TheFirstStep/src/com/revature/GoodByeEarth.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+import java.awt.print.Printable;
+
+public class GoodByeEarth {
+
+	public static void main(String[] args) {
+			System.out.println("goood bye!");
+	}
+
+}
