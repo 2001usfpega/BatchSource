2527a114dcb94e58b95e2ca4ccb8472e953687b9
Jacob Davis <jacob.davis@revature.com>
2020-01-14 13:13:14 -0500

    Java projects added.

---
+++ ProjectMayhem/src/com/revature/mayhem/Driver.java
@@ -0,0 +1,13 @@
+package com.revature.mayhem;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 46;
+		int c = a + b;
+		
+		System.out.print(c);
+	}
+
+}
