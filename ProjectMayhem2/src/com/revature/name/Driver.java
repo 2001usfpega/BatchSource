8cc70a3031dbc69042e96cf3e4aeb27f7c79acbf
Kaelin <kaelinmdickerson@gmail.com>
2020-01-15 00:49:19 +0000

    added everything to be pulled down to windows

---
+++ ProjectMayhem2/src/com/revature/name/Driver.java
@@ -0,0 +1,45 @@
+package com.revature.name;
+
+import com.revature.bean.Person; //<--Fully Qualified Class Name
+
+public class Driver {
+	//single line comment
+	/*
+	 * Multi
+	 * line
+	 * comment
+	 * lol
+	 * 
+	 * Naming Conventions
+	 * Classes and Projects- Pascal casing ex. ProjectMayhem
+	 * methods and variables- Camel casing ex. firstSecondThird
+	 * package- all lower case, separated by periods
+	 * constants - ALL_CAPS_LOLZ
+	 */
+	//Main method - serves as the entry point for an application
+	public static void main(String [] args) {
+	//public- access modifier errbody can "see" it
+	//static- belongs to class, no object needed
+	//void- doesn't return anything
+	//main- name of method
+	//String [] args- an array of strings called args
+	int a = 3;
+	int b = 46;
+	int c = a + b;
+	System.out.println("I like to eat " + c + " tacos");
+	/*
+	 * Members of a class- can have different forms
+	 * Variables, Methods, Constructors
+	 * instance variables- property belongs to specific object
+	 * static variables- belong to class/all instances
+	 * instance methods- behavior related to a specific object
+	 * static method- relative to entire class
+	 * Constructors- Instatntiates the class using the keyword "new"
+	 */
+	Person p = new Person();
+	p.setName("Jim");
+	System.out.println(p);
+	Person q = new Person("BobPam", 81, 27);
+	System.out.println(q);
+	}
+}
---
+++ TheFirstStep/src/com/revature/GoodbyeEarth.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+public class GoodbyeEarth {
+
+	public static void main(String[] args) {
+		System.out.print("Goodbye Earth");
+
+
+	}
+
+}
