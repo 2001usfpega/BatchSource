1d1a59aba0e7bbb73d63d713f8cf22ac349067c5
nexxgodd <crazy_nuts_2006@yahoo.com>
2020-01-16 21:37:27 -0500

    some stuff

---
+++ CoreJavaAssignment/src/com/revature/homework/Classy.java
@@ -0,0 +1,15 @@
+package com.revature.homework;
+
+public class Classy {
+
+	public static void main(String ... cheese) {
+		bubbleSort(1,0,5,6,3,2,3,7,9,8,4);
+	}
+
+	//Q1
+	public static void bubbleSort(int ... i) {
+		
+		
+		System.out.println("done!");
+	}
+}
